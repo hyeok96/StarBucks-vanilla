c69eec683f9914342f67b2beaf5cf88971910ef8
dktmskf <saing0813@naver.com>
2021-12-07 21:02:10 +0900

    Create badges and use javascript library
    Create badges and used lodash, gsap

--- js/main.js
+++ js/main.js
@@ -14,3 +14,23 @@ inputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   inputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges')
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY)
+  if (window.scrollY > 500) {
+    // 배너 숨기기
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none',
+    });
+  } else {
+    // 배너 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block',
+    });
+  };
+}, 300));
+// _.throttle()
